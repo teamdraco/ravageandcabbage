a78e599923897a51e93165aa73ca12781a6ac06f
Coda <cody.koelliker@outlook.com>
2022-04-27 03:44:29 -0600

    fix rendertypes

--- src/main/java/teamdraco/ravagecabbage/client/render/CabbagerRenderer.java
+++ src/main/java/teamdraco/ravagecabbage/client/render/CabbagerRenderer.java
@@ -1,14 +1,26 @@
 package teamdraco.ravagecabbage.client.render;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 import teamdraco.ravagecabbage.client.model.CabbagerModel;
 import teamdraco.ravagecabbage.common.entities.CabbagerEntity;
+import teamdraco.ravagecabbage.common.entities.RCRavagerEntity;
 
 public class CabbagerRenderer extends GeoEntityRenderer<CabbagerEntity> {
 
     public CabbagerRenderer(EntityRendererProvider.Context context) {
         super(context, new CabbagerModel());
         this.shadowRadius = 0.5F;
+    }
+
+    @Override
+    public RenderType getRenderType(CabbagerEntity animatable, float partialTicks, PoseStack stack, @Nullable MultiBufferSource renderTypeBuffer, @Nullable VertexConsumer vertexBuilder, int packedLightIn, ResourceLocation textureLocation) {
+        return RenderType.entityTranslucent(textureLocation);
     }
 }
--- src/main/java/teamdraco/ravagecabbage/client/render/RCRavagerRenderer.java
+++ src/main/java/teamdraco/ravagecabbage/client/render/RCRavagerRenderer.java
@@ -1,7 +1,12 @@
 package teamdraco.ravagecabbage.client.render;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 import teamdraco.ravagecabbage.RavageAndCabbage;
 import teamdraco.ravagecabbage.client.model.RCRavagerModel;
@@ -17,5 +22,10 @@ public class RCRavagerRenderer extends GeoEntityRenderer<RCRavagerEntity> {
         this.addLayer(new RCSaddleLayer<>(this, SADDLE_TEXTURE));
         this.addLayer(new RCRavagerHornLayer(this));
         this.shadowRadius = 1.1F;
+    }
+
+    @Override
+    public RenderType getRenderType(RCRavagerEntity animatable, float partialTicks, PoseStack stack, @Nullable MultiBufferSource renderTypeBuffer, @Nullable VertexConsumer vertexBuilder, int packedLightIn, ResourceLocation textureLocation) {
+        return RenderType.entityTranslucent(textureLocation);
     }
 }
